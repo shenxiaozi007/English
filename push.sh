26a26d1def3e40f860162794bb5dad0f88b4b260
hxc <173315279@qq.com>
2020-04-12 18:13:46 +0800

    test

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 message=$1
 book="2020-04-12-my-english-book.md"
+book_bak="2020-04-12-my-english-book-bak.md"
 #更新自己的分支
 echo "更新开始！"
 cp my-english-book.md ../hxc.github.io/_posts/$book
@@ -13,8 +14,9 @@ echo "更新完成！"
 echo "更新博客开始！"
 
 cd ../hxc.github.io/_posts/
-#添加固定的头
+cp $book $book_bak
 
+#添加固定的头
 echo "---
 layout:     post                    # 使用的布局（不需要改）
 title:      My First Post               # 标题 
@@ -26,6 +28,10 @@ catalog: true                       # 是否归档
 tags:                               #标签
     - 生活
 ---" > $book 
+#写入内容
+echo $book_bak >> $book
+#干掉备份文件
+rm -f $book_bak
 git add ./
 git commit -m "$message"
 git push origin master
